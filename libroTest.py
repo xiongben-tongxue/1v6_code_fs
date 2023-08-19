0a4dfac2140346735382670a2838ffe158da5cbd
wangdong <wangdongdev@gmail.com>
2023-08-19 11:54:39 +0800

    完成代码环境的整理完成

--- libroTest.py
+++ libroTest.py
@@ -6,8 +6,11 @@ import project_function
 # 使用函数提取音频
 video_path = "C:/Test/0001.mp4"
 audio_output_path = "C:/Test/0001.wav"
-project_function.extract_audio_from_video(video_path, audio_output_path)
+#project_function.extract_audio_from_video(video_path, audio_output_path)
 
 
-filename = audio_output_path
+filename = video_path
 y, sr = librosa.load(filename, sr=None)
+
+mel_spec = librosa.feature.melspectrogram(y=y,sr=sr,n_mels=128).T
+print(mel_spec.shape) # 25ms
